0b9b08d441cb9d138e76470d0280dea500ade555
vsreedhar3 <visreedhar@gatech.edu>
2021-10-01 01:53:42 -0400

    add switch screen functionality

--- src/main/java/com/example/judy/TowerDefenseApplication.java
+++ src/main/java/com/example/judy/TowerDefenseApplication.java
@@ -1,7 +1,10 @@
 package com.example.judy;
 
+import com.example.judy.controllers.InitialConfigScreenController;
+import com.example.judy.controllers.WelcomeScreenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,13 +14,42 @@ public class TowerDefenseApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
+
+        // getting loader and a pane for the welcome screen
+        FXMLLoader welcomePaneLoader = new FXMLLoader(
                 TowerDefenseApplication.class.getResource("screens/welcome-screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        scene.getRoot().setStyle("-fx-font-family: 'serif'");
-        stage.setTitle("Tower Defense Game");
-        stage.setScene(scene);
+        Parent welcomePane = welcomePaneLoader.load();
+        Scene welcomeScene = new Scene(welcomePane, 1280, 720);
+        welcomeScene.getRoot().setStyle("-fx-font-family: 'serif'");
+
+        // getting loader and a pane for the initial config screen
+        FXMLLoader initialConfigPaneLoader = new FXMLLoader(
+                TowerDefenseApplication.class.getResource("screens/initial-config-screen.fxml"));
+        Parent initialConfigPane = initialConfigPaneLoader.load();
+        Scene initialConfigScene = new Scene(initialConfigPane, 1280, 720);
+        initialConfigScene.getRoot().setStyle("-fx-font-family: 'serif'");
+
+        // getting loader and a pane for the initial game screen
+        FXMLLoader initialGamePaneLoader = new FXMLLoader(
+                TowerDefenseApplication.class.getResource("screens/initial-game-screen.fxml"));
+        Parent initialGamePane = initialGamePaneLoader.load();
+        Scene initialGameScene = new Scene(initialGamePane, 1280, 720);
+        initialGameScene.getRoot().setStyle("-fx-font-family: 'serif'");
+
+        // setting next scenes
+        WelcomeScreenController welcomeScreenController =
+                welcomePaneLoader.getController();
+        welcomeScreenController.setNextScene(initialConfigScene);
+
+        InitialConfigScreenController initialConfigScreenController =
+                initialConfigPaneLoader.getController();
+        initialConfigScreenController.setNextScene(initialGameScene);
+
+
+        stage.setTitle("Judy's Tower Defense Game");
+        stage.setScene(welcomeScene);
         stage.show();
+
     }
 
     /**
--- src/main/java/com/example/judy/controllers/InitialGameScreenController.java
+++ src/main/java/com/example/judy/controllers/InitialGameScreenController.java
@@ -1,14 +1,46 @@
 package com.example.judy.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class InitialGameScreenController {
     @FXML
-    private Label welcomeText;
+    private Label initialGameText;
+
+    private Scene nextScene;
+
+    @FXML
+    private void initialize() {
+        initialGameText.setText("Initial Game Screen");
+    }
+
+    /**
+     *
+     * Method to set next scene
+     *
+     * @param scene nextScene
+     */
+    public void setNextScene(Scene scene) {
+        nextScene = scene;
+    }
+
+    /**
+     *
+     * Method to switch screens
+     *
+     * @param actionEvent actionEvent to trigger screen switch
+     */
+    public void openNextScene(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(nextScene);
+    }
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        initialGameText.setText("Welcome to JavaFX Application!");
     }
 }
