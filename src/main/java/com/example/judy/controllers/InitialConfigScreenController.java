a63bebfdb01b14eb05bee3b4050ec0b3f7149e8a
Shively, James Lukas <jshively6@gatech.edu>
2021-10-03 00:13:55 -0400

    Giving functionality to the initial configuration screen

--- src/main/java/com/example/judy/controllers/InitialConfigScreenController.java
+++ src/main/java/com/example/judy/controllers/InitialConfigScreenController.java
@@ -1,30 +1,31 @@
 package com.example.judy.controllers;
-
-import com.example.judy.modules.Enemy;
 import com.example.judy.modules.Game;
-import com.example.judy.modules.Monument;
 import com.example.judy.modules.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-
 public class InitialConfigScreenController {
     @FXML
     private Label initialConfigText;
-
+    @FXML
+    private TextField nameInput;
+    @FXML
+    private Label nameText;
+    @FXML
+    private Label difficultyText;
     private Scene nextScene;
-    public Player player;
-    public Game game;
-    public Monument monument;
-    public Enemy enemy;
+    private String name;
+    private int difficulty = 0;
+    private Player player = new Player(null);
+    private Game game;
 
     @FXML
     private void initialize() {
-        initialConfigText.setText("Initial Config Screen");
+        initialConfigText.setText("Welcome to Save Judy! Please choose a name and difficulty.");
     }
 
     /**
@@ -49,13 +50,45 @@ public class InitialConfigScreenController {
     }
 
     @FXML
-    protected void onNextButtonClick(ActionEvent actionEvent) {
-        openNextScene(actionEvent);
+    protected void onStartClick(ActionEvent actionEvent) {
+        if (setGameConfigurations(name, difficulty)) {
+            openNextScene(actionEvent);
+        }
     }
 
-    public void setGameConfigurations(String name, int difficulty) {
+    public boolean setGameConfigurations(String name, int difficulty) {
+        if (name == null || difficulty == 0) {
+            return false;
+        }
         player = new Player(name);
         game = new Game(difficulty, player);
+        return true;
     }
 
+    @FXML
+    protected void onNameButtonClick(ActionEvent actionEvent) {
+        String name = nameInput.getText();
+        nameInput.clear();
+        if (name.isBlank()) {
+            initialConfigText.setText("Please enter a valid name.");
+        } else {
+            nameText.setText("Name: " + name);
+            this.name = name;
+        }
+    }
+
+    public void onEasyClick(ActionEvent actionEvent) {
+        difficultyText.setText("Difficulty: Easy");
+        difficulty = 1;
+    }
+
+    public void onMediumClick(ActionEvent actionEvent) {
+        difficultyText.setText("Difficulty: Medium");
+        difficulty = 2;
+    }
+
+    public void onHardClick(ActionEvent actionEvent) {
+        difficultyText.setText("Difficulty: Hard");
+        difficulty = 3;
+    }
 }
--- src/test/InitialConfigScreenTest.java
+++ src/test/InitialConfigScreenTest.java
@@ -4,7 +4,9 @@ import com.example.judy.controllers.InitialConfigScreenController;
 import com.example.judy.controllers.InitialGameScreenController;
 import com.example.judy.controllers.WelcomeScreenController;
 
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.testfx.framework.junit.ApplicationTest;
@@ -13,11 +15,40 @@ import org.testfx.matcher.base.NodeMatchers;
 
 import static org.testfx.api.FxAssert.verifyThat;
 
+/**
+ * Tests cases for the initial configuration screen in which players are supposed to select a name and difficulty.
+ */
 public class InitialConfigScreenTest extends ApplicationTest {
+    /**
+     * Starts up TowerDefenseApplication.
+     * @param stage Stage given to start
+     * @throws Exception Any exception
+     */
     @Override
     public void start(Stage stage) throws Exception {
         TowerDefenseApplication game = new TowerDefenseApplication();
         game.start(stage);
     }
 
+    /**
+     * Traverses to the initial configuration screen for testing.
+     */
+    @Before
+    public void traverseToInitialConfigScreen() {
+        clickOn("Next");
+    }
+
+    /**
+     * Tests to make sure that the player can move onto the initial
+     * game screen when they enter a valid name and difficulty.
+     */
+    @Test
+    public void testNameAndDifficulty() {
+        type(KeyCode.A);
+        clickOn("Enter");
+        clickOn("Easy");
+        clickOn("Start");
+
+        verifyThat("#initialGameText", NodeMatchers.isNotNull());
+    }
 }
